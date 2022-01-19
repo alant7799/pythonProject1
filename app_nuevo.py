190ea5f20943985b8b9c1b0276c9bd139bbecc21
alan1 <atoulouse@fi.uba.ar>
2022-01-19 09:30:30 -0300

    Se cambia el funcionamiento de la app.py, solo un index, no se despliega una segunda ventana para descargar

--- app_nuevo.py
+++ app_nuevo.py
@@ -22,6 +22,9 @@ def descargar():
 
     objeto_video = YouTube(str(_link))  # Para pytube esto es un objeto
 
+    path = objeto_video.streams.filter(progressive=True, file_extension='mp4').order_by(
+        'resolution').desc().first().download("uploads", filename=str(id_video) + ".mp4")
+
     if _audio == "Mp4":
         path = objeto_video.streams.filter(progressive=True, file_extension='mp4').order_by(
             'resolution').desc().first().download("uploads", filename=str(objeto_video.title) + ".mp4")
@@ -32,6 +35,7 @@ def descargar():
     print(path)
     return send_from_directory("uploads", path=str(objeto_video.title) + ".mp4", as_attachment=True)
 
+
 @app.route("/index")
 def inicio():
     return redirect("/")
@@ -40,4 +44,34 @@ def inicio():
 if __name__ == "__main__":
     app.run(debug=True)
 
+
+
+def return_files(id):
+    sql = "SELECT * FROM `main` WHERE `ID`=?;"
+    conn = sqlite3.connect("MiBaseDeDatos.sqlite")
+    cursor = conn.cursor()
+    id = str(id)
+    cursor.execute(sql, id)
+    lista_videos = cursor.fetchall()
+
+    print(lista_videos)
+    video = lista_videos[0]
+
+    id_video = video[0]
+    # foto_video = video[1]
+    # nombre_video = video[2]
+    url_video = video[3]
+    tipo_video = video[4]
+
+    objeto_video = YouTube(url_video)
+
+    if tipo_video == "Mp4":
+        path = objeto_video.streams.filter(progressive=True, file_extension='mp4').order_by(
+            'resolution').desc().first().download("uploads", filename=str(id_video) + ".mp4")
+    else:
+        path = (objeto_video.streams.filter(only_audio=True)[0]).download("uploads", filename=str(id_video) + ".mp4")
+
+    print(path)
+
+    return send_from_directory("uploads", path=str(id_video) + ".mp4", as_attachment=True)
 
